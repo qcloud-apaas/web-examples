7c5ab169c48a3531a91879a4907ce8a678ef3f2f
jasincere <772763242@qq.com>
2021-07-31 03:17:00 +0800

    chore(ts): datasource types

--- packages/datasource-property/src/DataSource.runmode.tsx
+++ packages/datasource-property/src/DataSource.runmode.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState, useMemo } from "react";
-import { SDK, DynamicDataSource } from "@qcloud-apaas/web-sdk";
+import {
+  SDK,
+  DynamicDataSource,
+  DataSourceResponse,
+} from "@qcloud-apaas/web-sdk";
 import DataTable from "./DataTable";
 
 type ExampleProps = {
@@ -11,7 +15,7 @@ const CustomRunComponent = (props: ExampleProps) => {
   const { dataSource, title } = props;
   const [tableRecords, setTableRecords] = useState([]);
   const [count, setCount] = useState(0);
-  const columns: any[] = useMemo(
+  const columns = useMemo(
     () =>
       dataSource.fields.map((field) => ({
         key: field?.code,
@@ -21,23 +25,23 @@ const CustomRunComponent = (props: ExampleProps) => {
   );
   useEffect(() => {
     const { type, variableType } = dataSource;
-    SDK.fetchByDataSource(dataSource)
+    SDK.fetchByDataSource<DataSourceResponse>(dataSource)
       .then((res) => {
         console.log(res);
         if (type === "database") {
           // 以数据库作为数据源
-          const { databaseResponse = {} } = res;
+          const { databaseResponse } = res;
           const { records, total } = databaseResponse;
           setTableRecords(records.map((record) => record.fieldValueMap));
           setCount(total);
         } else if (type === "flow" && variableType === "objectList") {
           // 以流程作为数据源，并且选择的流程结束节点类型 为 对象列表类型
-          const { flowResponses = [] } = res;
-          const response = flowResponses.find(
+          const { flowResponse = [] } = res;
+          const response = flowResponse.find(
             (item) => item.variableType === variableType
           );
           if (response && response.records) {
-            const { records, total } = res;
+            const { records, total } = response;
             setTableRecords(records.map((record) => record.fieldValueMap));
             setCount(total);
           }
