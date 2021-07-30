d1904fad31fb6fe35b767f8c62dc1d30a8c42784
jasincere <772763242@qq.com>
2021-07-31 03:04:53 +0800

    chore: datasource ts

--- packages/datasource-property/src/DataSource.designmode.tsx
+++ packages/datasource-property/src/DataSource.designmode.tsx
@@ -7,14 +7,11 @@ const CustomDesignComponent = (props) => {
   const { dataSource, title } = props;
   // SDK转换文本值
   const titleStr = SDK.transMetaVal2PureVal(title);
-  const { fields, entities, selectedEntity } = SDK.useDataSource(
-    dataSource,
-    {}
-  ) as any;
+  const { fields, entities, selectedEntity } = SDK.useDataSource(dataSource);
   // eslint-disable-next-line no-console
   console.log(entities);
   // eslint-disable-next-line no-console
-  const columns: any[] = fields.map((field) => ({
+  const columns = fields.map((field) => ({
     key: field?.code,
     name: field?.name,
   }));
